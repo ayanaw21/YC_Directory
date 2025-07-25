ea41ba9be8a074b792428b5462a1fde0e816e1ed
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-07-25 01:28:43 -0700

    finish github auth and local font

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,54 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { auth, signOut, signIn } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Navbar = () => {
-  return (
-    <header className='py-3 px-5 bg-white shadow-sm'>
-        <nav className='flex justify-between items-center'>
-            <Link href='/'>
-                <Image src='' alt='yc_directory'/>
-            </Link>
-        </nav>
-    </header>
-  )
-}
+const Navbar = async () => {
+	const session = await auth();
+	return (
+		<header className="py-3 px-5 bg-white shadow-sm">
+			<nav className="flex justify-between items-center">
+				<Link href="/">
+					<Image
+						src="/logo.png"
+						alt="yc_directory"
+						width={144}
+						height={30}
+					/>
+				</Link>
 
-export default Navbar
+				<div className="flex items-center gap-5 text-black">
+					{session && session?.user ? (
+						<>
+							<Link href="/startup/create">
+								<span>Create</span>
+							</Link>
+							<form
+								action={async () => {
+									"use server";
+									await signOut();
+								}}
+							>
+								<button type="submit">logout</button>
+							</form>
+							<Link href={`/user/${session?.id}`}>
+								<span>{session?.user?.name}</span>
+							</Link>
+						</>
+					) : (
+						<form
+							action={async () => {
+								"use server";
+								await signIn("github");
+							}}
+						>
+							<button type="submit">Signin</button>
+						</form>
+					)}
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
