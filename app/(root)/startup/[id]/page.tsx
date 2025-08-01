d8129295850836e7a1eb0a9c9f4a1191cec51304
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-08-01 03:32:08 -0700

    setup markdown contetn with style

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
-import MarkdownIt from 'markdown-it'
+import MarkdownIt from "markdown-it";
 
 const md = MarkdownIt();
 
@@ -16,7 +16,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 	const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 	if (!post) return notFound();
 
-	const parsedContent =  md.render(post?.pitch || '');
+	const parsedContent = md.render(post?.pitch || "");
 
 	return (
 		<>
@@ -55,12 +55,24 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 								</p>
 							</div>
 						</Link>
-                        <p className="category-tag">{post.category}</p>
+						<p className="category-tag">{post.category}</p>
 					</div>
-                    <h3 className="text-30-bold">
-                       Pitch Details 
-                    </h3>
+					<h3 className="text-30-bold">Pitch Details</h3>
+					{parsedContent ? (
+						<article
+						className="prose max-w-4xl font-work-sans break-all"
+							dangerouslySetInnerHTML={{ __html: parsedContent }}
+						/>
+					) : (
+						<p className="no-result">No details provided</p>
+					)}
 				</div>
+				<hr className="divider" />
+
+				{/* TODO : EDITOR SELECTED STARTUPS */}
+			</section>
+			<section>
+				 
 			</section>
 		</>
 	);
