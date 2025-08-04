3dcc146f184868c27bf8d5e59f3f259162aa1696
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-08-04 00:01:21 -0700

    fix import error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,8 @@
 import Navbar from "../../components/Navbar";
-
+import React from "react";
 export default function Layout({
 	children,
-}: Readonly<{ children: React.ReactNOde }>) {
+}: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<main className="font-work-sans">
 			<Navbar />
